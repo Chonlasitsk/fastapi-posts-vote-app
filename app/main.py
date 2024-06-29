a6e5ca095a4cfbeff0b241064805012890ffdfb0
Chonlasitsk <t.chonlasits@gmail.com>
2024-06-29 17:11:38 +0700

    add some text

--- app/main.py
+++ app/main.py
@@ -33,6 +33,6 @@ app.add_middleware(
 )
 @app.get("/")
 async def root():
-    return {"message": "Hello World to Ubuntu server !!"}
+    return {"message": "Hello World"}
 # if __name__ == "__main__":
 #     uvicorn.run('main:app', host="0.0.0.0", port=8001, reload=True)
